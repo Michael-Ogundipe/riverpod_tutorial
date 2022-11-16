a0f60b8a55f9feb71c49d217e103f6b40d921c47
syntax007 <ogundipeibukun51@gmail.com>
2022-11-15 16:13:36 -0800

    streamProvider

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const names = [
+  'Alice',
+  'Bob',
+  'Charlie',
+  'Diana',
+  'Eve',
+  'Fred',
+  'Gina',
+  'Harriet',
+  'Ilene',
+  'Joseph',
+  'Kincaid',
+  'Larry',
+];
+
+final tickerProvider = StreamProvider((ref) {
+  return Stream.periodic(
+    const Duration(
+      seconds: 1,
+    ),
+    (i) => i + 1,
+  );
+});
+
+final nameProvider = StreamProvider(
+  (ref) => ref.watch(tickerProvider.stream).map(
+        (count) => names.getRange(0, count),
+      ),
+);
+
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final names = ref.watch(nameProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('StreamProvider'),
+      ),
+      body: names.when(
+        data: (names){
+          return ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                    title: Text(names.elementAt(index)),
+              );
+            },
+          );
+        },
+        error: (error, stack) => const Text('Reached the end of the list!'),
+        loading: ()=> const Center(child: CircularProgressIndicator()),
       ),
     );
   }
