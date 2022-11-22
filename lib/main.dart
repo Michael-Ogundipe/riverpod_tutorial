5b8b6d7270a02dd836189791e4b47c7db2b34532
syntax007 <ogundipeibukun51@gmail.com>
2022-11-21 22:29:02 -0800

    example5

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -23,70 +21,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-@immutable
-class Person {
-  final String name;
-  final int age;
-  final String uuid;
-
-  Person({
-    required this.name,
-    required this.age,
-    String? uuid,
-  }) : uuid = uuid ?? const Uuid().v4();
-
-  Person updated([String? name, int? age]) {
-    return Person(
-      name: name ?? this.name,
-      age: age ?? this.age,
-      uuid: uuid,
-    );
-  }
-
-  String get displayName => '$name ($age years old)';
-
-  @override
-  bool operator ==(covariant Person other) => uuid == other.uuid;
-
-  @override
-  int get hashCode => uuid.hashCode;
-
-  @override
-  String toString() => 'Person(name: $name, age: $age, uuid: $uuid)';
-}
-
-class DataModel extends ChangeNotifier {
-  final List<Person> _people = [];
-
-  int get count => _people.length;
-
-  UnmodifiableListView<Person> get people => UnmodifiableListView(_people);
-
-  void add(Person person) {
-    _people.add(person);
-    notifyListeners();
-  }
-
-  void remove(Person person) {
-    _people.remove(person);
-    notifyListeners();
-  }
-
-  void update(Person updatedPerson) {
-    final index = _people.indexOf(updatedPerson);
-    final oldPerson = _people[index];
-    if (oldPerson.name != updatedPerson.name ||
-        oldPerson.age != updatedPerson.age) {
-      _people[index] = updatedPerson.updated(
-        updatedPerson.name,
-        updatedPerson.age,
-      );
-      notifyListeners();
-    }
-  }
-}
-
-final peopleProvider = ChangeNotifierProvider((ref) => DataModel());
 
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -97,103 +31,8 @@ class MyHomePage extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Consumer(builder: (context, ref, child) {
-        final dataModel = ref.watch(peopleProvider);
-        return ListView.builder(
-            itemCount: dataModel.count,
-            itemBuilder: (context, index) {
-              final person = dataModel.people[index];
-              return ListTile(
-                title: GestureDetector(
-                  onTap: () async {
-                    final updatedPerson = await createOrUpdatePersonDialog(context, person);
-                    if (updatedPerson != null) {
-                      dataModel.update(updatedPerson);
-                    }
-                  },
-                  child: Text(person.displayName),
-                ),
-              );
-            });
-      }),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () async {
-          final person = await createOrUpdatePersonDialog(context);
-          if (person != null) {
-            final dataModel = ref.read(peopleProvider);
-            dataModel.add(person);
-          }
-        },
-      ),
+      body: Column(),
     );
   }
 }
 
-final nameController = TextEditingController();
-final ageController = TextEditingController();
-
-Future<Person?> createOrUpdatePersonDialog(BuildContext context,
-    [Person? existingPerson]) {
-  String? name = existingPerson?.name;
-  int? age = existingPerson?.age;
-
-  nameController.text = name ?? '';
-  ageController.text = age?.toString() ?? '';
-
-  return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Create a person'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  labelText: 'Enter name here...',
-                ),
-                onChanged: (value) => name = value,
-              ),
-              TextField(
-                controller: ageController,
-                decoration: const InputDecoration(
-                  labelText: 'Enter age here...',
-                ),
-                onChanged: (value) => age = int.tryParse(value),
-              )
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                if (name != null && age != null) {
-                  if (existingPerson != null) {
-                    // have an existing person
-                    final newPerson = existingPerson.updated(name, age);
-                    Navigator.pop(context, newPerson);
-                  } else {
-                    // no existing person, create a new one
-                    Navigator.of(context).pop(
-                      Person(
-                        name: name!,
-                        age: age!,
-                      ),
-                    );
-                  }
-                } else {
-                  // no name or age or both
-                  Navigator.pop(context);
-                }
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        );
-      });
-}
